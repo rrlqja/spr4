27584f960a9614bbb453576f237daba2e66b65e0
song <dkclasltmf@naver.com>
2023-08-30 23:38:03 +0900

    result time aop 수정

--- src/main/java/song/spring4/aop/LogAspect.java
+++ src/main/java/song/spring4/aop/LogAspect.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StopWatch;
 
 @Slf4j
 public class LogAspect {
@@ -16,28 +15,76 @@ public class LogAspect {
     @Order(0)
     @Component
     @RequiredArgsConstructor
-    public static class log {
+    public static class ControllerAop {
 
-        @Around("song.spring4.aop.Pointcuts.servicePointcut()")
-        public Object serviceLog(ProceedingJoinPoint joinPoint) throws Throwable {
-            log.info("[Logging] {} ({})", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        @Around("song.spring4.aop.Pointcuts.controllerPointcut()")
+        public Object resultTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[Controller Aop] {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), joinPoint.getArgs());
 
             Object result;
-
-            Long resultTime;
-            StopWatch stopWatch = new StopWatch();
-            stopWatch.start();
+            long startTime = System.currentTimeMillis();
             try {
                 result = joinPoint.proceed();
             } catch (Exception e) {
                 throw e;
             } finally {
-                stopWatch.stop();
-
-                resultTime = stopWatch.getLastTaskTimeMillis();
+                long endTime = System.currentTimeMillis();
+                long resultTime = endTime - startTime;
+                log.info("[Controller Aop] result time = {}ms", resultTime);
             }
 
-            log.info("[Logging] result time = {}",resultTime);
+            return result;
+        }
+    }
+
+    @Aspect
+    @Order(1)
+    @Component
+    @RequiredArgsConstructor
+    public static class ServiceAop {
+
+        @Around("song.spring4.aop.Pointcuts.servicePointcut()")
+        public Object resultTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[Service Aop] {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), joinPoint.getArgs());
+
+            Object result;
+            long startTime = System.currentTimeMillis();
+            try {
+                result = joinPoint.proceed();
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                long endTime = System.currentTimeMillis();
+                long resultTime = endTime - startTime;
+                log.info("[Service Aop] result time = {}ms", resultTime);
+            }
+
+            return result;
+        }
+    }
+
+    @Aspect
+    @Order(2)
+    @Component
+    @RequiredArgsConstructor
+    public static class RepositoryAop {
+
+        @Around("song.spring4.aop.Pointcuts.repositoryPointcut()")
+        public Object resultTimeLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[Repository Aop] {}.{}({})", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), joinPoint.getArgs());
+
+            Object result;
+            long startTime = System.currentTimeMillis();
+            try {
+                result = joinPoint.proceed();
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                long endTime = System.currentTimeMillis();
+                long resultTime = endTime - startTime;
+                log.info("[Repository Aop] result time = {}ms", resultTime);
+            }
+
             return result;
         }
     }
--- src/main/java/song/spring4/aop/Pointcuts.java
+++ src/main/java/song/spring4/aop/Pointcuts.java
@@ -3,7 +3,15 @@ package song.spring4.aop;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class Pointcuts {
+    @Pointcut("execution(* song.spring4.controller.*.*(..))")
+    public void controllerPointcut() {
+    }
+
     @Pointcut("execution(* song.spring4.service.*.*(..))")
     public void servicePointcut() {
+    }
+
+    @Pointcut("execution(* song.spring4.repository.*.*(..))")
+    public void repositoryPointcut() {
     }
 }
