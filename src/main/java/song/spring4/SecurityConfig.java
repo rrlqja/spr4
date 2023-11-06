71c4455e5f7e28c8c29d6136c2dd98a331f49222
song windows <dkclasltmf@naver.com>
2023-11-06 16:40:51 +0900

    feat: 네이버 oauth2 설정

--- src/main/java/song/spring4/SecurityConfig.java
+++ src/main/java/song/spring4/SecurityConfig.java
@@ -2,18 +2,24 @@ package song.spring4;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import song.spring4.security.authentication.LoginFailureHandler;
 import song.spring4.security.authentication.LoginSuccessHandler;
 import song.spring4.entity.role.RoleName;
+import song.spring4.security.oauth2.OAuth2UserServiceImpl;
 
 import static org.springframework.security.web.util.matcher.RegexRequestMatcher.*;
 
@@ -23,12 +29,16 @@ import static org.springframework.security.web.util.matcher.RegexRequestMatcher.
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Autowired
+    OAuth2UserServiceImpl oAuth2UserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/", "/login", "logout", "/signup", "/css/**", "/*.ico", "/*.js", "/error").permitAll()
+                        .requestMatchers("/login/**").permitAll()
                         .requestMatchers(regexMatcher("^/board/[0-9]+$")).permitAll()
                         .requestMatchers("/board/**").authenticated()
                         .requestMatchers("/comment/**").authenticated()
@@ -43,7 +53,13 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
-                        .permitAll());
+                        .permitAll())
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/login-error")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)));
 
         return http.build();
     }
---
+++ src/main/java/song/spring4/security/oauth2/OAuth2UserServiceImpl.java
@@ -0,0 +1,32 @@
+package song.spring4.security.oauth2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+public class OAuth2UserServiceImpl implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        log.info("attributes ");
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        for (String s : attributes.keySet()) {
+            log.info("{}: {}", s, attributes.get(s).toString());
+        }
+
+        return oAuth2User;
+    }
+}
