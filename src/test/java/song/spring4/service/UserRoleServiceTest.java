3228b4eae688bc4e6d146d602274ed4ff038d32b
song windows <dkclasltmf@naver.com>
2023-12-04 22:02:10 +0900

    test: 사용자 역할 부여, 역할 회수 테스트

--- src/test/java/song/spring4/service/UserRoleServiceTest.java
+++ src/test/java/song/spring4/service/UserRoleServiceTest.java
@@ -1,34 +1,56 @@
 package song.spring4.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import song.spring4.entity.role.RoleName;
+import org.springframework.test.context.TestPropertySource;
+import song.spring4.domain.userrole.consts.Role;
 import song.spring4.repository.UserRoleJpaRepository;
-
-import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.profiles.active=test",
+        "JASYPT_ENCRYPTOR_PASSWORD=test"
+})
 class UserRoleServiceTest {
-
     @Autowired
     UserRoleService userRoleService;
     @Autowired
     UserRoleJpaRepository userRoleRepository;
 
+    @BeforeEach
+    void beforeEach() {
+        userRoleRepository.deleteAll();
+    }
+
+    @DisplayName("권한 부여")
     @Test
-    void save1() {
-//        userRoleService.grantRole(1L, RoleName.ROLE_ADMIN.name());
-//
-//        assertThat(userRoleRepository.findByUserIdAndRole(1L, RoleName.ROLE_ADMIN).isPresent()).isTrue();
-//
-//        userRoleService.revokeRole(1L, RoleName.ROLE_ADMIN);
-//
-//        assertThat(userRoleRepository.findByUserIdAndRole(1L, RoleName.ROLE_ADMIN).isEmpty()).isTrue();
+    void grantRoleTest() {
+        Long userId = 1L;
+        userRoleService.grantRole(userId, Role.ROLE_USER.name());
+
+        Assertions.assertThat(userRoleRepository.findByUserIdAndRole(userId, Role.ROLE_USER))
+                .isNotEmpty();
+    }
+
+    @DisplayName("권한 회수")
+    @Test
+    void revokeRoleTest() {
+        Long userId = 1L;
+        // grant role
+        userRoleService.grantRole(userId, Role.ROLE_USER.name());
+        Assertions.assertThat(userRoleRepository.findByUserIdAndRole(userId, Role.ROLE_USER))
+                .isNotEmpty();
+
+        // revoke role
+        userRoleService.revokeRole(userId, Role.ROLE_USER.name());
+        Assertions.assertThat(userRoleRepository.findByUserIdAndRole(userId, Role.ROLE_USER))
+                .isEmpty();
     }
 
 }
