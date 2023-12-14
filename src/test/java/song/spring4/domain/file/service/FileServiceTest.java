c50aadc99069e37d20ac4b9e4948a8b350ebe201
song windows <dkclasltmf@naver.com>
2023-12-14 22:42:06 +0900

    test: fileService 테스트

--- src/test/java/song/spring4/domain/file/service/FileServiceTest.java
+++ src/test/java/song/spring4/domain/file/service/FileServiceTest.java
@@ -1,6 +1,7 @@
 package song.spring4.domain.file.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +10,10 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.TestPropertySource;
 import song.spring4.domain.file.dto.UploadFileDto;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -24,12 +24,12 @@ import static org.assertj.core.api.Assertions.*;
 class FileServiceTest {
     @Autowired
     FileService fileService;
-
     @Value("${upload.path}")
     String path;
 
+    @DisplayName("파일 업로드")
     @Test
-    void upload1() throws IOException {
+    void uploadFile() throws IOException {
         String content = "mock";
         MockMultipartFile mockMultipartFile = new MockMultipartFile(
                 "file",
@@ -38,10 +38,21 @@ class FileServiceTest {
                 content.getBytes(StandardCharsets.UTF_8)
         );
 
+        assertDoesNotThrow(() -> fileService.upload(mockMultipartFile));
+    }
+
+    @DisplayName("파일 삭제")
+    @Test
+    void deleteFile() throws Exception {
+        String content = "mock";
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "file",
+                "mock.txt",
+                "text/plain",
+                content.getBytes(StandardCharsets.UTF_8)
+        );
         UploadFileDto uploadFileDto = fileService.upload(mockMultipartFile);
 
-        File file = new File(path + uploadFileDto.getSavedFileName());
-        assertThat(file.exists()).isTrue();
-        file.delete();
+        assertDoesNotThrow(() -> fileService.delete(uploadFileDto.getSavedFileName()));
     }
 }
