78714a3c5ee21bc1945b788c4eeb5fa6e2895d99
song windows <dkclasltmf@naver.com>
2023-12-14 14:15:34 +0900

    fix: dto 생성자 수정

--- src/main/java/song/spring4/domain/comment/dto/EditCommentDto.java
+++ src/main/java/song/spring4/domain/comment/dto/EditCommentDto.java
@@ -1,10 +1,12 @@
 package song.spring4.domain.comment.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import song.spring4.domain.comment.entity.Comment;
 
 @Getter @Setter
+@NoArgsConstructor
 public class EditCommentDto {
     private Long id;
     private String content;
